6a9a6ca5480187ec84a8300699744b21714a7332
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2024-02-01 19:55:38 +0200

    feat: some table update

--- src/analyse/counts/create_button.py
+++ src/analyse/counts/create_button.py
@@ -1,7 +1,10 @@
 def create_button(url):
     return f"""<div style="margin: auto; 
     background-color: #f0f0f0; 
-    padding: 10px; 
+    padding-left: 10px; 
+    padding-right: 10px; 
+    padding-top: 2px; 
+    padding-bottom: 2px; 
     border: 1px solid #a9bef3; 
     border-radius: 5px; 
     display: inline-block;">
