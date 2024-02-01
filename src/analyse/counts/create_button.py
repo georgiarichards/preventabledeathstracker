dfccb5738cb60823dfeab30bfd6d6246f4fc7a22
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2024-02-01 19:51:45 +0200

    feat: some table update

--- src/analyse/counts/create_button.py
+++ src/analyse/counts/create_button.py
@@ -1,7 +1,11 @@
 def create_button(url):
-    return f"""<div style="margin: auto; background-color: #f0f0f0; '
-                  'padding: 10px; border: 1px solid #a9bef3; border-radius: 5px; display: inline-block;">)
-          <a href="{url}" target="_blank" style="color: #0066cc; text-decoration: none; font-weight: bold;">
+    return f"""<div style="margin: auto; 
+    background-color: #f0f0f0; 
+    padding: 10px; 
+    border: 1px solid #a9bef3; 
+    border-radius: 5px; 
+    display: inline-block;">
+          <a href="{url}" style="color: #0066cc; text-decoration: none; font-weight: bold;">
             Report:
           </a>
         </div>"""
