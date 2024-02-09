5f4ea003d816b8c9e2f2bb48e78604e1f1b8b3de
SekachVitaliy <vitaly.sekach@gmail.com>
2024-02-09 19:29:07 +0200

    fix: remove empty links from Deceased name

--- src/analyse/counts/create_button.py
+++ src/analyse/counts/create_button.py
@@ -1,5 +1,5 @@
 def create_button(cell, url):
-    if "nan" in str(cell):
+    if "nan" == cell:
         return ""
     return f'<a href="{url}" style="color: #0066cc; text-decoration: none; font-weight: bold;">{cell}</a>'
 
