2d893e4f3bdf4da4ad5553c7189b3bfe9dbe4ce9
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2024-02-14 13:30:07 +0200

    feat: update titles

--- src/analyse/counts/name_counts.py
+++ src/analyse/counts/name_counts.py
@@ -84,10 +84,13 @@ top_years = name_counts[top_names]
 # ### Calculating the top coroner titles
 
 top_categorised = pd.DataFrame({"count": top_counts})
+# top_name_titles = top_counts.index.map(coroner_titles).fillna("Unknown")
+top_categorised = top_categorised.assign(Unknown=0)
 top_name_titles = top_counts.index.map(coroner_titles).fillna("Unknown")
-
 for title in top_name_titles.unique():
     top_categorised[title] = top_categorised["count"].where(top_name_titles == title, 0)
+
+top_categorised.rename(columns={'Unknown': 'Unknown title'}, inplace=True)
 
 # %% [markdown]
 # ### Calculating counts for titles
