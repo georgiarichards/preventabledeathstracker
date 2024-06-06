3840b8d1c05c2708541f21243e409108180cdbfe
SekachVitaliy <vitaly.sekach@gmail.com>
2024-06-06 22:22:11 +0300

    fix: add logs

--- src/stripe_handler/OrdersHandler.py
+++ src/stripe_handler/OrdersHandler.py
@@ -18,15 +18,26 @@ class OrdersHandler:
         self.email_cred = UserCred(email=os.getenv("EMAIL"), password=os.getenv("PASSWORD"))
 
     def handle(self) -> None:
-        start_time = int((datetime.now() - timedelta(minutes=60)).timestamp())
-        end_time = int(datetime.now().timestamp())
+        start_time = int((datetime.utcnow() - timedelta(minutes=60)).timestamp())
+        end_time = int(datetime.utcnow().timestamp())
         payment_intents = stripe.PaymentIntent.list()
+        start_time_str = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+        end_time_str = datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+
+        print(f"Start Time: {start_time_str}")
+        print(f"End Time: {end_time_str}")
         # Doesn't work for some reason  created={"gte": start_time, "lte": end_time})
         _orders = {}
         for payment in payment_intents:
             if not (start_time <= payment.created <= end_time):
+                created_time_str = datetime.fromtimestamp(payment.created).strftime('%Y-%m-%d %H:%M:%S')
+                print(f"Processing {payment.id} skipped, it's been over 1 hour since it was created {created_time_str}")
                 continue
-            if payment.status != "succeeded" or not payment.invoice:
+            if payment.status != "succeeded":
+                print(f"Processing {payment.id} skipped, payment.status {payment.status}")
+                continue
+            elif not payment.invoice:
+                print(f"Processing {payment.id} skipped, invoice not created")
                 continue
             print(payment)
             invoice = stripe.Invoice.retrieve(payment.invoice)
@@ -34,6 +45,7 @@ class OrdersHandler:
             customer_name = invoice.customer_name
             product = invoice.lines.data[0].description
             if product not in PRODUCT_DETAILS_MAP:
+                print(f"Processing {payment.id} {product} not checked")
                 continue
             product_path = PRODUCT_DETAILS_MAP[product]["path"]
             print(f"{customer_email} - {product} - path: {product_path}")
