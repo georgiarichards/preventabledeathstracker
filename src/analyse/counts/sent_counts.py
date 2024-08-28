279be247c206e22a4ae6f39f04fa14958f9c9b4b
SekachVitaliy <50178320+SekachVitaliy@users.noreply.github.com>
2024-08-28 12:36:03 +0300

    feat: add new fields

--- src/analyse/counts/sent_counts.py
+++ src/analyse/counts/sent_counts.py
@@ -522,8 +522,8 @@ new_types = sent_types_with_partial.copy()
 new_types.index.rename("Addressed to", inplace=True)
 new_types = new_types[["% completed", "completed", "partial", "overdue", "pending"]]
 result = pd.merge(sent_to_yearly, new_types, on='Addressed to', how='inner')
-result.sort_values(by="Total no. PFDs", ascending=False, inplace=True)
 result.rename(columns={"Total no. PFDs": "Total"}, inplace=True)
+result.sort_values(by=["% completed", "Total"], ascending=[False, False], inplace=True)
 result.to_csv(f"{DATA_PATH}/sent/sent_to_yearly.csv")
 
 top_types_with_partial = sent_types_with_partial.loc[top_counts.index]
